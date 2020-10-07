11b5f170e0c383de4ac873a02092b1c692a03dd0
Polina <polinaucc@gmail.com>
2020-10-07 17:28:21 +0300

    added the ability to turn on devices

--- src/main/java/ua/polina/smart_house_sensors/SensorController.java
+++ src/main/java/ua/polina/smart_house_sensors/SensorController.java
@@ -2,7 +2,6 @@ package ua.polina.smart_house_sensors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import ua.polina.smart_house_sensors.api.DeviceRoomListApi;
 import ua.polina.smart_house_sensors.service.DeviceRoomService;
@@ -21,5 +20,12 @@ public class SensorController {
     @GetMapping(value = "/get-on-devices/{room-id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public DeviceRoomListApi getOnRoomDevices(@PathVariable("room-id") Long roomId) {
         return new DeviceRoomListApi(deviceRoomService.getAll());
+    }
+
+    @ResponseBody
+    @GetMapping("/on-device/{device-room-id}")
+    public Boolean onDevice(@PathVariable("device-room-id") Long deviceRoomId) {
+        if (deviceRoomService.onDevice(deviceRoomId) != null) return true;
+        else return false;
     }
 }
--- src/main/java/ua/polina/smart_house_sensors/service/DeviceRoomService.java
+++ src/main/java/ua/polina/smart_house_sensors/service/DeviceRoomService.java
@@ -3,8 +3,11 @@ package ua.polina.smart_house_sensors.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ua.polina.smart_house_sensors.entity.DeviceRoom;
+import ua.polina.smart_house_sensors.entity.State;
 import ua.polina.smart_house_sensors.repository.DeviceRoomRepository;
 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 
 @Service
@@ -16,7 +19,15 @@ public class DeviceRoomService {
         this.deviceRoomRepository = deviceRoomRepository;
     }
 
-    public List<DeviceRoom> getAll(){
+    public List<DeviceRoom> getAll() {
         return deviceRoomRepository.findAll();
+    }
+
+    public DeviceRoom onDevice(Long deviceRoomId) {
+        DeviceRoom deviceRoom = deviceRoomRepository.findById(deviceRoomId)
+                .orElseThrow(() -> new IllegalArgumentException("No such device"));
+        deviceRoom.setState(State.ON);
+        deviceRoom.setOnTime(LocalTime.now());
+        return deviceRoomRepository.save(deviceRoom);
     }
 }
