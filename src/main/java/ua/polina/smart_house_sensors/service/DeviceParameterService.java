a60f1ab816e636a5daec2bb8c4991ff7704fbaec
Polina <polinaucc@gmail.com>
2020-10-08 17:34:23 +0300

    added check for device parameter value

--- src/main/java/ua/polina/smart_house_sensors/service/DeviceParameterService.java
+++ src/main/java/ua/polina/smart_house_sensors/service/DeviceParameterService.java
@@ -27,16 +27,23 @@ public class DeviceParameterService {
     }
 
     /**
-     * Sets up new value for device parameter.
+     * Sets up a new value for device parameter, if it is more than minimum
+     * value and less than maximum value.
      *
      * @param setUpParameterDto the set up parameter dto
      * @return saved device parameter
      */
-    public DeviceParameter setUpParameterValue(SetUpParameterDto setUpParameterDto){
+    public DeviceParameter setUpParameterValue(SetUpParameterDto setUpParameterDto) {
         DeviceParameter deviceParameter
                 = deviceParameterRepository.findById(setUpParameterDto.getParameterId())
-                .orElseThrow(()->new IllegalArgumentException("No device parameter with such id"));
-        deviceParameter.setValue(setUpParameterDto.getValue());
+                .orElseThrow(() -> new IllegalArgumentException("No device parameter with such id"));
+        if (setUpParameterDto.getValue() < deviceParameter.getMinTheoreticalValue()) {
+            deviceParameter.setValue(deviceParameter.getMinTheoreticalValue());
+        } else if (setUpParameterDto.getValue() > deviceParameter.getMaxTheoreticalValue()) {
+            deviceParameter.setValue(deviceParameter.getMaxTheoreticalValue());
+        } else {
+            deviceParameter.setValue(setUpParameterDto.getValue());
+        }
         return deviceParameterRepository.save(deviceParameter);
     }
 }
